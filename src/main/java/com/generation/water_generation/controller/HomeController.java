3012e7159155a0e1b954a1dd343c79a112cc5acf
mayu-code <durbulemayu@gmail.com>
2025-01-29 22:07:33 +0530

    push

--- src/main/java/com/generation/water_generation/controller/HomeController.java
+++ src/main/java/com/generation/water_generation/controller/HomeController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +22,7 @@ import com.generation.water_generation.services.SensorDataServiceImpl;
 
 @RestController
 @RequestMapping("/")
+@CrossOrigin
 public class HomeController {
 
     @Autowired
